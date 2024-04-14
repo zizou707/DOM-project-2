cd0d352e92c52c8b2406f7a8e4f8818abe38cd55
zizou707 <162337899+zizou707@users.noreply.github.com>
2024-04-14 02:02:38 +0200

    txt change

--- src/color-change.js
+++ src/color-change.js
@@ -1,7 +1,7 @@
 //declaring variables :
 const box = document.querySelector(".color-box") ;
 const btn = document.querySelector("#change-color-btn") ;
-//Generate new random color :
+//creating function that generate new random color :
 const ColorChange = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     box.style.backgroundColor = "#" + randomColor;
